f67abfa48215795cccb93a693d5d4e88c7219688
Luis Lavena <luis@area17.com>
2012-12-03 17:30:25 -0500

    Modify engine initializer to work with Rails 3.2
    The engine initializer was not being properly loaded during asset
    precompilation, causing "inmutable index" exceptions and halting
    the precompilation during a deploy.
    
    This modification allow engine gets properly initialized and
    preprocessor registered prior asset precompilation.

--- lib/i18n/js/engine.rb
+++ lib/i18n/js/engine.rb
@@ -3,14 +3,14 @@ require "i18n/js"
 module I18n
   module JS
     class Engine < ::Rails::Engine
-      initializer :after => "sprockets.environment" do
+      initializer "i18n-js.register_preprocessor", :after => "sprockets.environment" do |app|
         ActiveSupport.on_load(:after_initialize, :yield => true) do
           next unless JS.has_asset_pipeline?
-          next unless Rails.configuration.assets.compile
+          next unless app.config.assets.compile
 
-          Rails.application.assets.register_preprocessor "application/javascript", :"i18n-js_dependencies" do |context, source|
+          app.assets.register_preprocessor "application/javascript", nil do |context, source|
             next source unless context.logical_path == "i18n/translations"
-            ::I18n.load_path.each {|path| context.depend_on(path)}
+            ::I18n.load_path.each { |path| context.depend_on(path) }
             source
           end
         end
